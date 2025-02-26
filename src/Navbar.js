ebdbedf71c1f78457ac709e5d8fcdde893fea1d4
nati1207 <natalia.cristiani.russo.7@gmail.com>
2025-02-26 14:55:14 -0300

    Criação

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import{Link} from 'react-router-dom';
+
+const Navbar = () => {
+    return ( <nav className="navbar">
+        <h1>the dojo blog</h1>
+        <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/create">New blog</Link>
+        </div>
+    </nav> );
+}
+ 
+export default Navbar;
